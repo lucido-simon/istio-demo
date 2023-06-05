36d26006e2c495a6da5b63cbbce2c83fdcb34494
Simon LUCIDO <simon.lucido@etu.umontpellier.fr>
2023-06-05 13:01:51 +0200

    chore: update to latest k3d config version
    Signed-off-by: Simon LUCIDO <simon.lucido@etu.umontpellier.fr>

--- ops/terraform/prod/k8s-cluster/variables.tf
+++ ops/terraform/prod/k8s-cluster/variables.tf
@@ -1,9 +1,9 @@
 variable "kubeconfig_context_name" {
   type    = string
-  default = "k3d-inventory-poc"
+  default = "k3d-istio-demo"
 }
 
 variable "kubeconfig_path" {
   type    = string
-  default = "~/.kube/inventory-poc"
+  default = "~/.kube/istio-demo"
 }
